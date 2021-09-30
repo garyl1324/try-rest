c7354eb42aa8933081d511867e61fcf274c7b0e4
garyl1324 <garyl1324@gmail.com>
2021-09-30 14:06:44 -0400

    epic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-
-
+import Axios from "axios"
 
 const App = () =>{
-
+    const getJoke = () => {
+        Axios.get("https://icanhazdadjoke.com/"
+        ).then(
+            (response) => {
+                console.log(response);
+            }
+        );
+    };
  return (
-
- <div className="container">
-
- <p>garrison leach</p>
-
- </div>
-
+<div className="container"><button onClick={getJoke}>Dad Joke</button>
+<br/><p>Hi!</p>
+    </div>
  );
-
 }
 
 
