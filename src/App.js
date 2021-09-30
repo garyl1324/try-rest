16b1918caf77b13b28a4a3ac7205a41a690d73ff
garyl1324 <garyl1324@gmail.com>
2021-09-30 14:36:38 -0400

    batman

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import Axios from "axios"
+import React, { useState } from 'react';
 
 const App = () =>{
+    const [joke, setJoke] = useState()
     const getJoke = () => {
-        Axios.get("https://icanhazdadjoke.com/"
+        Axios.get("https://icanhazdadjoke.com/",{
+            headers: {
+              "Accept": "application/json"
+            }
+          }
         ).then(
             (response) => {
-                console.log(response);
+                //console.log(response);
+                setJoke(response.data.joke);
             }
         );
     };
  return (
 <div className="container"><button onClick={getJoke}>Dad Joke</button>
-<br/><p>Hi!</p>
+<br/><h3>{joke}</h3>
     </div>
  );
 }
